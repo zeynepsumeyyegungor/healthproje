d4d94d04ef08ae59789a59f363384500ca1def26
ismail mert demirci <ismailmertdemirci@gmail.com>
2022-11-16 20:59:53 +0300

    16.11.2022

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,25 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/html-reports/cucumber.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-reports/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        features = "src/test/resources/features/register.feature",
+        glue = {"stepDefinitions"},
+        tags = "@",
+        dryRun = false
+
+)
+public class Runner {
+
+}
