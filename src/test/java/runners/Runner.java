4656148089e907fbfe49e7b8984a0cb542cefb92
Caner Karakoyun <caner.karakoyun@gmail.com>
2022-11-24 20:39:13 +0300

    24.11.2022 20:38

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml" },
         features = "src/test/resources/features/UI_features",
         glue = "stepDefinitions",
-        tags = "@US08_TC01",
+        tags = "@US08_ TC01",
         dryRun = false
 
 )
